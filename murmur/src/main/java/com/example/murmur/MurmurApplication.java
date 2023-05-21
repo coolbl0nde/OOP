5d75f183496bf50f2ab4a298297001dbe8928d0e
Marya <Marya@DESKTOP-TDKCMV7>
2023-05-21 13:28:29 +0300

    finished registrarion and authorization

--- murmur/src/main/java/com/example/murmur/MurmurApplication.java
+++ murmur/src/main/java/com/example/murmur/MurmurApplication.java
@@ -1,17 +1,12 @@
 package com.example.murmur;
 import com.example.murmur.managers.UserManager;
-import com.example.repositories.UserRepository;
 
-import com.example.repositories.MessageRepository;
 import com.example.murmur.entities.Chat;
 import com.example.murmur.entities.User;
 import com.example.murmur.managers.ChatManager;
 import jdk.dynalink.linker.LinkerServices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class MurmurApplication {
@@ -20,29 +15,6 @@ public class MurmurApplication {
 
         SpringApplication.run(MurmurApplication.class, args);
 
-        /*String user1 = User.createUser("Masha", "Mihalevich", "+375294575445", "coolbl0nde", "364674537");
-        String user2 = User.createUser("Ilya", "Mihalevich", "+375295748934", "ilfkdg", "kkfjfdjglgjf");
-
-        System.out.println("user1: " + user1);
-        System.out.println("user2: " + user2);
-
-        List<String> users = new ArrayList<>();
-
-        users.add(user1);
-        users.add(user2);
-
-        String chat = Chat.createChat(users);
-        System.out.println("Создан чат 1 и 2 пользователя: " + chat);
-
-        System.out.println("message1: " + ChatManager.sendMessage("hello", user1, chat));
-        System.out.println("message2: " + ChatManager.sendMessage("bye", user2, chat));
-
-        System.out.println("messages: " + ChatManager.messagesList(chat));
-        System.out.println("name user1: " + UserManager.getName(user1));*/
-
-        //ChatManager.deleteMessage("64691c29a8c253541d6dff4c", "64691c29a8c253541d6dff4b");
-
-        System.out.println(UserManager.findUser("+375294575445"));
     }
 
 }
--- murmur/src/main/java/com/example/murmur/controllers/AuthController.java
+++ murmur/src/main/java/com/example/murmur/controllers/AuthController.java
@@ -1,6 +1,7 @@
 package com.example.murmur.controllers;
 
 import com.example.murmur.entities.User;
+import com.example.murmur.managers.UserManager;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,27 +9,56 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 @RestController
 public class AuthController {
 
     @PostMapping("/api/register")
     public ResponseEntity<String> register(@RequestBody Map<String, String> user){
-        //check
+        String regex = "\\+\\d{7,12}";
 
-        if (user.isEmpty()) {
+        if (user != null) {
+            Map<String, Object> phoneNumber = UserManager.findUser(user.get("phoneNumber"));
+            Map<String, Object> username = UserManager.findUser(user.get("username"));
+
+            if(!user.get("phoneNumber").matches(regex)){
+                return ResponseEntity.internalServerError().body("Incorrect phone number");
+            }
+
+            if(phoneNumber != null){
+                return ResponseEntity.internalServerError().body("User with this phone number already exists");
+            }else if(username != null){
+                return ResponseEntity.internalServerError().body("User with this username already exists");
+            }else{
+                String hashedPassword = BCrypt.hashpw(user.get("password"), BCrypt.gensalt());
+
+                return (ResponseEntity<String>) ResponseEntity.ok(User.createUser(user.get("name"), user.get("surname"),
+                        user.get("phoneNumber"), user.get("username"), hashedPassword));
+            }
+        }else{
             return ResponseEntity.notFound().build();
-        } else {
-            return (ResponseEntity<String>) ResponseEntity.ok(User.createUser(user.get("name"), user.get("surname"),
-                    user.get("phoneNumber"), user.get("username"), user.get("password")));
         }
     }
 
     @PostMapping("api/auth")
     public ResponseEntity<String> auth(@RequestBody Map<String, Object> user){
-        if(user.isEmpty()){
-            return ResponseEntity.notFound().build();
+
+        if(user != null){
+            Map<String, Object> userExist = UserManager.findUser((String) user.get("phoneNumber"));
+
+            if(userExist != null && userExist.get("username").equals(user.get("username"))){
+
+                if(BCrypt.checkpw((String) user.get("password"), (String) userExist.get("password"))){
+                    return ResponseEntity.ok("success");
+                }else{
+                    return ResponseEntity.internalServerError().body("Incorrect password");
+                }
+            }else{
+                return ResponseEntity.internalServerError().body("Incorrect username or phone number");
+            }
         }else{
-            return ResponseEntity.ok("success");
+            return ResponseEntity.badRequest().build();
         }
     }
 
